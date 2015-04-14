14aec09d01f804b414bb98d7e869992bb9076ed9
WZrk <wrivers@kippnyccp.org>
2015-04-14 08:41:31 -0400

    Update application.js

--- Shopping/application.js
+++ Shopping/application.js
@@ -1,3 +1,22 @@
-$('button').on('click',function(){
-  
-})
+$(document).ready(function(){
+  var CurrentIndex = 100
+  var click_count = 0
+  randomIndex = Math.floor(Math.random()*100);
+    $('button').on('click', function(){
+    var userGuess = $('input').val();
+    click_count = click_count+1 ;
+    $('#guess_counter')
+    console.log(click_count)
+    console.log(userGuess)
+    if (userGuess == randomIndex){
+      alert("You win")
+    }
+    else if (userGuess > randomIndex){
+      alert("Guess Lower!")
+    }
+    else if (userGuess < randomIndex){
+      alert("Guess Higher!")
+    }
+  });
+});
+
