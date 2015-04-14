453cb4065ea864f0b506c168fa560a08e57156c4
WZrk <wrivers@kippnyccp.org>
2015-04-14 08:47:05 -0400

    Update application.js

--- Guess_My_Number/application.js
+++ Guess_My_Number/application.js
@@ -1,38 +1,22 @@
 $(document).ready(function(){
-  //find a way for the program to choose a random number between 1 and 100, save this as a variable
-  var number=Math.floor(Math.random()*100)
-  console.log(number)
-  
-  //when the player clicks on the 'guess' button
+  var CurrentIndex = 100
+  var click_count = 0
+  randomIndex = Math.floor(Math.random()*100);
   $('button').on('click', function(){
-    $('#feedback').empty()
-    var guess=parseint($('input').val())
-    //save their guess as a variable
-    
-    
-    if (guess===number)
-    //and compare this guess to the random number that the computer picked
-    //if the user guessed the correct number...
-    {
-      $('#feedback').append('Correct...nice.')
+    var userGuess = $('input').val();
+    click_count = click_count+1 ;
+    $('#guess_counter')
+    console.log(click_count)
+    console.log(userGuess)
+    if (userGuess == randomIndex){
+      alert("You win")
     }
-      //what happens if the guess is correct?
-    else
-      if (guess>number)
-      {
-        $('#feedback').append('Number's too high...')
-      }
-      else
-      {
-        $("#feedback").append("Number's too low...");
-      }
-    //if the user guessed too high...
-    
-      //update the 'feedback' paragraph to tell them to guess lower
-      
-    //otherwise, the user guessed too low...
-    
-      //update the 'feedback' paragraph to tell them to guess higher
-    
-  });
+   else if (userGuess > randomIndex){
+      alert("Guess Lower!")
+    }
+    else if (userGuess < randomIndex){
+      alert("Guess Higher!")
+    }
+    });
 });
+
