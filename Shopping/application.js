b8e097d7835c1b665d1790aeb2bb7dfe62e76a97
WZrk <wrivers@kippnyccp.org>
2015-04-15 09:26:32 -0400

    Update application.js

--- Shopping/application.js
+++ Shopping/application.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
     var entry = $('input').val();
     $('ul').append("<li>"+entry+"</li>"); //the item will be appended into the ul as a li element.
   })
+  $('#clear').on('click', function(){
+    var entry = $('input').val();
+    $("ul").remove();
+  })
 });
 
