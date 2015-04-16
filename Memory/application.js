98b38a1d74ff0972655e5770fe204cdb26003235
WZrk <wrivers@kippnyccp.org>
2015-04-16 08:44:01 -0400

    Update application.js

--- Memory/application.js
+++ Memory/application.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
-
+  var card_array = ["Touka", "Yoshimoro", "Hinami", "Kaneki", "Touka", "Rize", "Juzo", "Rize", "Yoshimoro", "Amon", "Juzo", "Hinami", "Kaneki", "Amon", "Uta", "Tsukiyama", "Mado", "Tsukiyama", "Uta", "Mado"];
+  var click_count = 0;
+  var click_1 = 'nothing';
+  var click_2 = 'nothing';
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
-  }
+  };
+  $('.card').click(function(){
+    if(click_count == 0){ //When the person clicks on the first card
+  	$(this).find('p').css('opacity', 1);
+    	$(this).find('p').addClass('clicked');  
+      	click_count = 1;
+      	click_1 = $(this).find('p').text();
+    }
+    else{	// when the person clicks on the second card
+      $(this).find('p').css('opacity', 1);
+      click_count = 0;
+      click_2 = $(this).find('p').text();
+      $(this).find('p').addClass('clicked');
+      if(click_1 == click_2){
+        $('.clicked').css('color', 'blue').removeClass('clicked');
+      }
+      else{//if not the same then disappear
+         setTimeout(function(){$('.clicked').css('opacity', '0').removeClass('clicked')}, 1000);
+      };
+    };
+  });
 });
+
