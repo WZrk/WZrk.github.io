37afe7345bffdc0804be1b926f3c73d3f46c4a70
WZrk <wrivers@kippnyccp.org>
2015-05-12 08:50:05 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,1 +1,24 @@
 from random import random
+total_wins = 0
+for i in range(10000):
+  region wins = 0
+  region 1 = random()
+  if region 1 <= .87:
+    region wins = region wins + 1
+  else:
+    region wins = region wins
+  region 2 = random()
+  if region 2 <= .65:
+    region wins = region wins + 1
+  else:
+    region wins = region wins
+  region 3 = random()
+  if region 3 <= .17:
+    region wins = region wins + 1
+  else:
+    region wins = region wins
+    
+  if region wins >= 2:
+    print 
+  else:
+    print "You did not win the election..."
